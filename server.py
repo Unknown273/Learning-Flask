a1b890a75c82a3045695094f16d1dffcf61b2474
Yusuf Kazi <1485724+Unknown273@users.noreply.github.com>
2023-03-25 15:49:59 +0530

    Made a seperate home and login_signup page

--- server.py
+++ server.py
@@ -3,8 +3,12 @@ from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
+def root():
+  return render_template('home.html')
+
+@app.route('/login_signup')
 def index():
-  return render_template("index.html")
+  return render_template("login_signup.html")
 
 if __name__ == "__main__":
   app.run()
